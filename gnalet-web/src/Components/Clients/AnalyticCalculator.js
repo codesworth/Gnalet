380df36d0c0877faeb9c134f4b9e4fbbdcc01abf
Codesworth <codezworth@gmail.com>
2019-02-11 12:26:26 +0000

    Refactored Analytics 02

--- gnalet-web/src/Components/Clients/AnalyticCalculator.js
+++ gnalet-web/src/Components/Clients/AnalyticCalculator.js
@@ -1,65 +1,159 @@
-
-import {AN_TOTALS, AN_YEAR_DATA, FIELD_SOLVED, FIELD_FLAGGED, FIELD_DUPLICATE} from '../../Helpers/Constants'
-
-function makeAnalyticForCategoryData(category){
-    const data = {
-        pending:0,unsolved:0,solved:0,flag:0, duplicate:0
-    }
-    //console.log("Initial data: ", data);
-    let alltotal = 0;
-    for (const id in data){
-        let total = 0;
-        for (const region in category){
-            if(region !== 'key'){
-                const value = category[region];
-                if (typeof(value) == 'object'){
-                    total  = total + value[id]
-                }
-            }
+import {
+  AN_TOTALS,
+  AN_YEAR_DATA,
+  FIELD_SOLVED,
+  FIELD_FLAGGED,
+  FIELD_DUPLICATE,
+  category_ids
+} from "../../Helpers/Constants";
+/**
+ * Calculates Analytics for various  Adminstrative Jurisdictions irrespective of categories
+ * @param {*} bigdata Query Snapshot from Database
+ * @returns JS Nested Object with Administrative Assemlies as outer Keys and statuses as nested Keys
+ */
+function makeAnalyticForRegionData(regionalData) {
+  const data = {
+    pending: 0,
+    unsolved: 0,
+    solved: 0,
+    flag: 0,
+    duplicate: 0
+  };
+  //console.log("Initial data: ", data);
+  let alltotal = 0;
+  for (const id in data) {
+    let total = 0;
+    for (const category in regionalData) {
+      if (category !== "key") {
+        const value = regionalData[category];
+        if (typeof value == "object") {
+          total = total + value[id];
         }
-        data[id] = total;
+      }
     }
-    data.key = category.key;
-    const {pending,unsolved,solved,flag,duplicate} = data;
-    alltotal = pending + unsolved + solved + flag + duplicate;
-    let comp = ((solved) / (alltotal - flag)) * 100;
-    if (solved === 0 || (alltotal - (flag + duplicate)) === 0){
-        comp = 0;
-    }
-    data.completion = comp.toFixed(2)
-    data.total = alltotal
-    return data;
+    data[id] = total;
+  }
+  data.key = regionalData.key;
+  const { pending, unsolved, solved, flag, duplicate } = data;
+  alltotal = pending + unsolved + solved + flag + duplicate;
+  let comp = (solved / (alltotal - flag)) * 100;
+  if (solved === 0 || alltotal - (flag + duplicate) === 0) {
+    comp = 0;
+  }
+  data.completion = comp.toFixed(2);
+  data.total = alltotal;
+  return data;
 }
 
-
-export function makeBigdataAnalytics(bigdata){
-    const retdata = {}
-    const data = []
-    const totals = {pending:0,unsolved:0,solved:0,flag:0,total:0, duplicate:0}
-    bigdata.forEach(element => {
-        //console.log("The eleemt is: ", element);
-        const catdata = makeAnalyticForCategoryData(element);
-        for (const key in totals){
-            let val = totals[key];
-            val = val + catdata[key];
-            totals[key] = val;
-        }
-        data.push(catdata);
-    });
-    let comp = (totals[FIELD_SOLVED]) / (totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE])) * 100
-    if ((totals[FIELD_SOLVED]) === 0 || (totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE])) === 0){
-        comp = 0
+export function makeBigdataAnalytics(bigdata) {
+  const retdata = {};
+  const data = [];
+  const totals = {
+    pending: 0,
+    unsolved: 0,
+    solved: 0,
+    flag: 0,
+    total: 0,
+    duplicate: 0
+  };
+  bigdata.forEach(element => {
+    //console.log("The eleemt is: ", element);
+    const catdata = makeAnalyticForRegionData(element);
+    for (const key in totals) {
+      let val = totals[key];
+      val = val + catdata[key];
+      totals[key] = val;
     }
+    data.push(catdata);
+  });
+  let comp =
+    (totals[FIELD_SOLVED] /
+      (totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE]))) *
+    100;
+  if (
+    totals[FIELD_SOLVED] === 0 ||
+    totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE]) === 0
+  ) {
+    comp = 0;
+  }
 
-    totals.completion = comp.toFixed(1);
-    retdata[AN_YEAR_DATA] = data;
-    retdata[AN_TOTALS] = totals;
-    
-    return retdata;
+  totals.completion = comp.toFixed(1);
+  retdata[AN_YEAR_DATA] = data;
+  retdata[AN_TOTALS] = totals;
+
+  return retdata;
 }
 
+/**
+ * Calculates Analytics for various Issue Categories across all Adminstrative Jurisdictions
+ * @param {*} bigdata Query Snapshot from Database
+ * @returns JS Nested Object with categories as outer Keys and statuses as nested Keys
+ */
 
-function collapseAllTotals(data){
-    const totals = {pending:0,unsolved:0,solved:0,flag:0}
+export function makeBigdataAnalyticsForCategories(bigdata) {
+  const retdata = {};
+  const data = [];
+  const categories = category_ids;
+  const totals = {
+    pending: 0,
+    unsolved: 0,
+    solved: 0,
+    flag: 0,
+    total: 0,
+    duplicate: 0
+  };
+  categories.forEach(id => {
+    retdata[id] = {
+      pending: 0,
+      unsolved: 0,
+      solved: 0,
+      flag: 0,
+      total: 0,
+      duplicate: 0
+    };
+  });
+  bigdata.forEach(element => {
+    for (const id in element) {
+      console.log(`Element id is: ${id}`);
+      let data = element[id];
+      for (const stat in data) {
+        if (id === "key") break;
+        console.log("Retid", retdata[id]);
+        console.log("Stat is", stat);
+        const old = retdata[id][stat];
+        retdata[id][stat] = old + data[stat];
+      }
+    }
+  });
 
+  console.log("The catdata: ", retdata);
+  //console.log("The eleemt is: ", element);
+  //   const catdata = makeAnalyticForRegionData(element);
+  //   for (const key in totals) {
+  //     let val = totals[key];
+  //     val = val + catdata[key];
+  //     totals[key] = val;
+  //   }
+  //   data.push(catdata);
+  // });
+  // let comp =
+  //   (totals[FIELD_SOLVED] /
+  //     (totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE]))) *
+  //   100;
+  // if (
+  //   totals[FIELD_SOLVED] === 0 ||
+  //   totals.total - (totals[FIELD_FLAGGED] + totals[FIELD_DUPLICATE]) === 0
+  // ) {
+  //   comp = 0;
+  // }
+
+  // totals.completion = comp.toFixed(1);
+  // retdata[AN_YEAR_DATA] = data;
+  // retdata[AN_TOTALS] = totals;
+
+  return retdata;
+}
+
+function collapseAllTotals(data) {
+  const totals = { pending: 0, unsolved: 0, solved: 0, flag: 0 };
 }
--- gnalet-web/src/Components/Clients/Analytics.js
+++ gnalet-web/src/Components/Clients/Analytics.js
@@ -1,114 +1,140 @@
-import React, { Component } from 'react'
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firestoreConnect, withFirestore} from 'react-redux-firebase';
-import { REF_ANALYTICS, FIELD_UNSOLVED, FIELD_PENDING, FIELD_SOLVED, FIELD_FLAGGED, FIELD_DUPLICATE } from '../../Helpers/Constants';
-import Spinner from '../layout/Spinner';
+import React, { Component } from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect, withFirestore } from "react-redux-firebase";
+import {
+  REF_ANALYTICS,
+  FIELD_UNSOLVED,
+  FIELD_PENDING,
+  FIELD_SOLVED,
+  FIELD_FLAGGED,
+  FIELD_DUPLICATE
+} from "../../Helpers/Constants";
+import Spinner from "../layout/Spinner";
 
-import { makeBigdataAnalytics } from './AnalyticCalculator';
+import {
+  makeBigdataAnalytics,
+  makeBigdataAnalyticsForCategories
+} from "./AnalyticCalculator";
 
 class Analytics extends Component {
+  state = {
+    bigdata: null
+  };
 
-    state = {
-        bigdata:null
-    }
-
-    componentWillMount(){
-        const {firestore} = this.props;
-        const nbigdata = [];
-        firestore.collection(REF_ANALYTICS).get().then(bigdata => {
-            bigdata.forEach(element => {
-                const data = element.data();
-                data.key = element.id;
-                nbigdata.push(data);
-            });
-            const analysed = makeBigdataAnalytics(nbigdata);
-            console.log("This is big data: ", analysed);
-            this.setState({bigdata:analysed})
-        })
-
-    }
-
-
+  componentWillMount() {
+    const { firestore } = this.props;
+    const nbigdata = [];
+    firestore
+      .collection(REF_ANALYTICS)
+      .get()
+      .then(bigdata => {
+        bigdata.forEach(element => {
+          const data = element.data();
+          data.key = element.id;
+          nbigdata.push(data);
+        });
+        const analysed = makeBigdataAnalytics(nbigdata);
+        const a = makeBigdataAnalyticsForCategories(nbigdata);
+        console.log("This is big data: ", nbigdata);
+        this.setState({ bigdata: analysed });
+      });
+  }
 
   render() {
-      const {bigdata} = this.state
-      
-      if (bigdata){
-        const {yearly,totals} = bigdata;
-        return (
-            <div className="col-md-10">
-              <div className='row mg'>
-                  <div className="col">
-                      <div className="card">
-                          <div className="card-header">
-                              <h3>REPORT ANALYTICS</h3>
-                          </div>
-                          <div className="card-body">
-                              
-                              <table className="table table-bordered table-striped">
-                                  <thead>
-                                  <tr>
-                                  <th>CATEGORY</th>
-                                  <th>UNSOLVED</th>
-                                  <th>PENDING</th>
-                                  <th>SOLVED</th>
-                                  <th>FLAGGED</th> 
-                                  <th>DUPLICATE</th>
-                                  <th>TOTAL</th>
-                                  <th>% COMPLETION</th>
-                                  </tr>
-                                  </thead>
-      
-                                  <tbody>
-                                      {yearly.map(data => (
-                                          <tr key={data.key}>
-                                              <td>
-                                                  {data.key} 
-                                              </td>
-                                              <td>
-                                                  {data[FIELD_UNSOLVED]}
-                                              </td>
-                                              <td>
-                                                  {data[FIELD_PENDING]}
-                                              </td>
-                                              <td>{data[FIELD_SOLVED]}</td>
-                                              <td>{data[FIELD_FLAGGED]}</td>
-                                              <td>{data[FIELD_DUPLICATE]}</td>
-                                              <td>{data.total}</td>
-                                              <td>{data.completion}%</td>
-                                          </tr>
-                                      ))}
-                                      <tr key="total">
-                                        <td><h5>TOTALS</h5></td>
-                                        <td><h5>{totals[FIELD_UNSOLVED]}</h5></td>
-                                        <td><h5>{totals[FIELD_PENDING]}</h5></td>
-                                        <td><h5>{totals[FIELD_SOLVED]}</h5></td>
-                                        <td><h5>{totals[FIELD_FLAGGED]}</h5></td>
-                                        <td><h5>{totals[FIELD_DUPLICATE]}</h5></td>
-                                        <td><h5>{totals.total}</h5></td>
-                                        <td><h5>{totals.completion}%</h5></td>
-                                      </tr>
-                                  </tbody>
-                              </table>
-                              </div>
-                          </div>
-                  </div>
-              </div> 
+    const { bigdata } = this.state;
+
+    if (bigdata) {
+      const { yearly, totals } = bigdata;
+      return (
+        <div className="col-md-10">
+          <div className="row mg">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  <h3>REPORT ANALYTICS</h3>
+                </div>
+                <div className="card-body">
+                  <table className="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>CATEGORY</th>
+                        <th>UNSOLVED</th>
+                        <th>PENDING</th>
+                        <th>SOLVED</th>
+                        <th>FLAGGED</th>
+                        <th>DUPLICATE</th>
+                        <th>TOTAL</th>
+                        <th>% COMPLETION</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {yearly.map(data => (
+                        <tr key={data.key}>
+                          <td>{this.shortenString(data.key)}</td>
+                          <td>{data[FIELD_UNSOLVED]}</td>
+                          <td>{data[FIELD_PENDING]}</td>
+                          <td>{data[FIELD_SOLVED]}</td>
+                          <td>{data[FIELD_FLAGGED]}</td>
+                          <td>{data[FIELD_DUPLICATE]}</td>
+                          <td>{data.total}</td>
+                          <td>{data.completion}%</td>
+                        </tr>
+                      ))}
+                      <tr key="total">
+                        <td>
+                          <h5>TOTALS</h5>
+                        </td>
+                        <td>
+                          <h5>{totals[FIELD_UNSOLVED]}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals[FIELD_PENDING]}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals[FIELD_SOLVED]}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals[FIELD_FLAGGED]}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals[FIELD_DUPLICATE]}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals.total}</h5>
+                        </td>
+                        <td>
+                          <h5>{totals.completion}%</h5>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
-          )
-      }else{
-          return (
-              <Spinner/>
-          )
-      }
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
+  }
+
+  shortenString(str) {
+    const arrstr = str.split(" ");
+    if (arrstr.length > 2) {
+      return arrstr[0].concat(" ").concat(arrstr[1]);
+    }
+    return str;
   }
 }
 
-
 export default compose(
-    firestoreConnect(), withFirestore,connect((state, props) => ({
-        auth: state.firebase.auth,
-        settings: state.settings
-    }))
-)(Analytics)
+  firestoreConnect(),
+  withFirestore,
+  connect((state, props) => ({
+    auth: state.firebase.auth,
+    settings: state.settings
+  }))
+)(Analytics);
