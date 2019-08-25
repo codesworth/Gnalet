995eb5a55310f36a60ccd80b0d5c2fa8257e946d
Codesworth <codezworth@gmail.com>
2019-08-25 11:02:38 +0000

    Added Config

---
+++ gnalet-web/src/config/config.js
@@ -0,0 +1,22 @@
+const ProjectConfig = {};
+
+ProjectConfig.default = {
+  apiKey: "AIzaSyDK06vnoYsjRLRbZKsKd4zpS72SbVydXqg",
+  authDomain: "gnalet-e91c4.firebaseapp.com",
+  databaseURL: "https://gnalet-e91c4.firebaseio.com",
+  projectId: "gnalet-e91c4",
+  storageBucket: "gnalet-e91c4.appspot.com",
+  messagingSenderId: "1026593999626"
+};
+
+ProjectConfig.citifm = {
+  apiKey: "AIzaSyC-Hym_N5yeTw1xX4EghTNvgiEn4jRAe78",
+  authDomain: "gnalet-citifm.firebaseapp.com",
+  databaseURL: "https://gnalet-citifm.firebaseio.com",
+  projectId: "gnalet-citifm",
+  storageBucket: "",
+  messagingSenderId: "870007677042",
+  appId: "1:870007677042:web:3160ed2136c0877b"
+};
+
+export default ProjectConfig;
